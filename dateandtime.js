ce9d5752d6ceabcca5532c5862938d2194d1dadc
Sanketpatel22 <sanketsutariya1482@gmail.com>
2025-05-13 11:54:46 +0530

    Date and Timr

---
+++ dateandtime.js
@@ -0,0 +1,19 @@
+//dates 
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date('2023-10-01');
+console.log(myCreatedDate.toDateString());
+
+
+let MyTimeStamp = Date.now();
+console.log(MyTimeStamp.lo);
