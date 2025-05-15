572cb92d2bdbfc13aa2f815f30b9715e4282ec94
Sanketpatel22 <sanketsutariya1482@gmail.com>
2025-05-15 15:54:46 +0530

    iteration part two

--- part_3/iteration.js
+++ part_3/iteration.js
@@ -1,30 +1,45 @@
 // for loop
 
-for (let i = 0; i < 10 ; i++) {
+// for (let i = 0; i < 10 ; i++) {
 
-    console.log(i);
+//     console.log(i);
 
-    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
      
-    for (let i = 0; i < array.length;  i++) {
-        console.log(array[i]);
+//     for (let i = 0; i < array.length;  i++) {
+//         console.log(array[i]);
         
-    }
-}
+//     }
+// }
 
-//break and continue
+// //break and continue
 
-//break : it will break the loop
-//continue : it will skip the current iteration and move to next iteration
+// //break : it will break the loop
+// //continue : it will skip the current iteration and move to next iteration
 
 
-for (let i = 0; i < 10 ; i++) {
+// for (let i = 0; i < 10 ; i++) {
     
-    if (i === 5) {
-        console.log("5 detected");
-        break;
-    }
-    console.log(i);
+//     if (i === 5) {
+//         console.log("5 detected");
+//         break;
+//     }
+//     console.log(i);
 
 
+// }
+
+// while loop
+
+let index = 0;
+while (index < 10) {
+    console.log("index: ", index);
+    index++;  
 }
+
+// do loop
+let score = 0;
+do {
+    console.log("score: ", score);
+    score++;
+} while (score < 10);
